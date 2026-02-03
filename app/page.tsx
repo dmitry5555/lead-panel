87017b4e67edc0d386f5f21aa135a76cde837bb5
dmitrii <mikrolux@gmail.com>
2026-02-03 19:42:39 +0700

    new cool design

--- app/page.tsx
+++ app/page.tsx
@@ -12,17 +12,19 @@ import LeadRow from "@/components/LeadRow"
 
 export default function Home() {
   return (
-    <div className="flex flex-col bg-[#F4F4EE] divide-y divide-[#E5E7EB]">
-      <div className="flex gap-4 px-8 py-3 w-full font-medium text-stone-100 bg-stone-900/90 border-b border-stone-800">
+    <div className="flex flex-col max-w-screen-lg mx-auto font-roboto">
+
+      <div className="flex px-8 pt-8 pb-6 w-full font-medium text-slate-400 ">
         <div className="w-1/12">ID</div>
-        <div className="w-2/12">Источник</div>
         <div className="w-2/12">Дата</div>
         <div className="w-2/12">Camp.Name</div>
         <div className="w-2/12">Creat.ID</div>
         <div className="w-2/12">Статус</div>
       </div>
-      <LeadRow />
-      <LeadRow />
+      <div className="rounded-md border border-[#F4F4EE]">
+        <LeadRow />
+        <LeadRow />
+      </div>
     </div>
     
   );
